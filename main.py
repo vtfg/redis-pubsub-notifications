9a9d2beb798960009fad6d4ec718692518d019b1
Victor Guedes <victorfelipe.guedes@hotmail.com>
2025-06-05 21:16:52 -0300

    feat: add get notifications endpoint

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/{channel}", response_class=StreamingResponse)
+async def get_notifications(channel: str | None) -> StreamingResponse:
+    return StreamingResponse(
+        _get_notifications(channel),
+        media_type="text/event-stream",
+        headers={
+            "Content-Type": "text/event-stream",
+            "Content-Encoding": "none",
+        },
+    )
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def _get_notifications(channel: str) -> List[str]:
+    notifications = {"1": ["fizz"], "2": ["buzz"]}
+
+    if channel in notifications:
+        return notifications[channel]
+
+    raise HTTPException(status_code=404, detail=f"Channel {channel} not found.")

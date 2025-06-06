28f7b483784534bdc78e86aedc343c2b14c1690b
Victor Guedes <victorfelipe.guedes@hotmail.com>
2025-06-06 00:39:03 -0300

    refactor: use pub/sub listen function instead of while loop

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 from typing import AsyncGenerator, Annotated
 
 from dotenv import load_dotenv
@@ -10,15 +9,10 @@ from utils import get_env
 
 load_dotenv()
 
-config = {
-    "REDIS_HOST": get_env("REDIS_HOST"),
-    "REDIS_PORT": get_env("REDIS_PORT"),
-    "REDIS_PASSWORD": get_env("REDIS_PASSWORD"),
-}
 app = FastAPI()
 
-client = Redis(
-    host=config.get("REDIS_HOST"), port=config.get("REDIS_PORT"), password=config.get("REDIS_PASSWORD"), ssl=True
+redis = Redis(
+    host=get_env("REDIS_HOST"), port=get_env("REDIS_PORT", int), password=get_env("REDIS_PASSWORD"), ssl=True
 )
 
 
@@ -26,29 +20,21 @@ client = Redis(
 async def get_notifications(channel: str | None) -> StreamingResponse:
     return StreamingResponse(
         _get_notifications(channel),
-        media_type="text/event-stream",
-        headers={
-            "Content-Type": "text/event-stream",
-            "Content-Encoding": "none",
-        },
+        media_type="text/event-stream"
     )
 
 
 async def _get_notifications(channel: str) -> AsyncGenerator[str, None]:
-    async with client.pubsub() as pubsub:
+    async with redis.pubsub() as pubsub:
         await pubsub.subscribe(f"notifications/{channel}")
 
-        while True:
-            message = await pubsub.get_message(ignore_subscribe_messages=True)
+        async for message in pubsub.listen():
+            if message["type"] == "message":
+                data: str = message["data"].decode("utf-8")
 
-            if message:
-                data: bytes = message["data"].decode("utf-8")
-
-                yield f"event: message\ndata: {str(data)}\n\n"
-
-                await asyncio.sleep(1)
+                yield f"event: message\ndata: {data}\n\n"
 
 
 @app.post("/{channel}", status_code=204)
 async def post_notification(channel: str, message: Annotated[str, Body(embed=True)]) -> None:
-    await client.publish(f"notifications/{channel}", message)
+    await redis.publish(f"notifications/{channel}", message)
